9c191796ebf7b784f6000a7899c57391855eb9cd
tall-horse <54848630+tall-horse@users.noreply.github.com>
2023-08-25 14:23:02 +0200

    Tick

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -16,7 +16,6 @@ void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
 
-	SetActorLocation(MyVector);
 }
 
 // Called every frame
@@ -24,5 +23,8 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	MyVector.Y += 1;
+
+	SetActorLocation(MyVector);
 }
 
